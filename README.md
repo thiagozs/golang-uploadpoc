9a928f2be2646b664e8875b969dbfb1f2132d60b
Thiago Zilli <thiago.zilli@gmail.com>
2017-06-23 15:08:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Go Upload Multi files POC
+---
+
+***Proof of concept*** of upload files with *golang* and *templates*.
+
+### Run the project
+```sh
+$ go run main.go
+```
+
+Open te browser on http://localhost:4300
+Have fun!
+
+---
+
+The MIT License (MIT)
+
+Copyright (c) 2017 THIAGO ZILLI SARMENTO
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+)
+
+//Compile templates on start.
+var templates = template.Must(template.ParseFiles("tmpl/upload.html"))
+
+//Display template name.
+func display(w http.ResponseWriter, tmpl string, data interface{}) {
+	templates.ExecuteTemplate(w, tmpl+".html", data)
+}
+
+//uploadHandler function.
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	//GET displays upload form.
+	case "GET":
+		display(w, "upload", nil)
+
+	//POST takes the uploaded and saves it to disk.
+	case "POST":
+		//parse the multipart form in the request
+		err := r.ParseMultipartForm(100000)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		//get a ref to the parsed multipart form
+		m := r.MultipartForm
+
+		//get the *fileheaders
+		files := m.File["myfiles"]
+
+		//each one by one
+		for i, _ := range files {
+
+			file, err := files[i].Open()
+			defer file.Close()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			//create destination file making sure the path is writeable.
+			dst, err := os.Create("/tmp/" + files[i].Filename)
+			defer dst.Close()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			//copy the uploaded file to the destination file
+			if _, err := io.Copy(dst, file); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+		}
+
+		//display success message.
+		display(w, "upload", "Upload successful.")
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func main() {
+	http.HandleFunc("/upload", uploadHandler)
+
+	//static file handler.
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	//Listen on port 8080
+	http.ListenAndServe(":4300", nil)
+}
